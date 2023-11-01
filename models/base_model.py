4047d56cc6b418b14494db9f49c8ec2069d6ee9a
lucieruin <lucie.wattez@hotmail.fr>
2023-11-01 14:54:35 +0100

    add arguments args and kwargs  for def init in class BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,19 @@ from datetime import datetime
 
 class BaseModel():
     """ common attributes & methods """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ initialization method """
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs :
+            for key, mydate in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    mydate = datetime.strptime(mydate, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, mydate)
+                elif key != "__class__":
+                    setattr(self, key, mydate)
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ print object """
