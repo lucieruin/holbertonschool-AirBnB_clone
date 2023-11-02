bc8c9bd5539de760aec57f6d3bbdbf1fb7937446
mickaeltartar <mickael.tartar@hotmail.com>
2023-11-02 13:12:57 +0100

    add comment & space

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
+""" module for console """
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     """ define hbnb command class """
@@ -9,13 +10,14 @@ class HBNBCommand(cmd.Cmd):
 
     def emptyline(self):
         return False
-    
+
     def do_EOF(self, line):
         """ EOF exit the program """
         return True
-    
+
     def do_quit(self, line):
         return True
-    
+
+
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
